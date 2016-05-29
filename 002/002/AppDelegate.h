4f13d6a332b6fa9d9c1453493cfc1d5803268ef0
shang513121645 <513121645@qq.com>
2016-05-29 20:21:49 +0800

    tibia测试

---
+++ 002/002/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  002
+//
+//  Created by 尚诚诚 on 16/5/29.
+//  Copyright © 2016年 尚诚诚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 002/002/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  002
+//
+//  Created by 尚诚诚 on 16/5/29.
+//  Copyright © 2016年 尚诚诚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
